cf9e890fe450b9567146a99e7a3d5a3c9bf4b80d
HomayounFakhar <71987668+HomayounFakhar@users.noreply.github.com>
2021-03-09 22:33:04 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './css/index.css'
 import App from './components/App'
+import EndPage from './components/EndPage.js'
+import Sizeproduct from './components/Sizeproduct.js'
+
 
 ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<EndPage />, document.getElementById('EndPage'))
+ReactDOM.render(<Sizeproduct />, document.getElementById('Sizeproduct'))
