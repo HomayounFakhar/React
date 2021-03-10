b0975138c08ad3236a395b97630a2d382eba0c1d
HomayounFakhar <71987668+HomayounFakhar@users.noreply.github.com>
2021-03-10 03:08:23 -0500

    Add Color

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import './css/index.css'
 import App from './components/App'
 import EndPage from './components/EndPage.js'
 import Sizeproduct from './components/Sizeproduct.js'
+import Colorproduct from './components/Colorproduct.js'
 
 
 ReactDOM.render(<App />, document.getElementById('root'))
 ReactDOM.render(<EndPage />, document.getElementById('EndPage'))
 ReactDOM.render(<Sizeproduct />, document.getElementById('Sizeproduct'))
+ReactDOM.render(<Colorproduct />, document.getElementById('Colorproduct'))
